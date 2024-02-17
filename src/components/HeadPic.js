d321d1834a4ddc6f4b26381509c00deec14f407b
ishhanayy <ishanimalviya333@gmail.com>
2024-02-17 17:03:36 +0530

    the changes made in main

--- src/components/HeadPic.js
+++ src/components/HeadPic.js
@@ -1,11 +1,22 @@
-import React from "react"
-import Leaf from '../img/Leaf.png'
+import React from "react";
+import Leaf from "../img/Leaf.png";
+import Bohemian from "../img/Bohemian.png";
 
-function Pic()
-{
-    return <div class="div2" id="expandable-image"> <img class="image" src={Leaf} alt="sustainable message"/> 
-    <script src="./scroll.js"></script>
+function Pic() {
+  return (
+    <div class="div2" id="expandable-image">
+      <div className="drifting-container">
+        <img
+          className="BohemianPic drifting-image"
+          src={Bohemian}
+          alt="bohemian"
+        />
+      </div>
+      <div>
+        <img class="image" src={Leaf} alt="sustainable message" />
+      </div>
     </div>
+  );
 }
 
 export default Pic;
